c405d3a438945a64acdf9a97b41179f935d4c653
r12a <ishida@w3.org>
2018-06-04 09:15:25 +0100

    Archive slices directory

---
+++ slices/workarea/small/fname2hex16.py
@@ -0,0 +1,38 @@
+import os, string, glob, sys
+
+if len(sys.argv) > 1:
+    hexOfFirst = sys.argv[1];
+else:
+    print 'Hex number of first character:'
+    hexOfFirst = sys.stdin.readline()[:-1];
+
+filenames = glob.glob('*.png')
+for filename in filenames:
+	if len(filename) == 18: 
+		correctedname = filename[:12]+'0'+filename[12:]
+		os.rename( filename, correctedname );
+
+
+filenames = glob.glob('*.png')
+filenames.sort()
+decnum = int(hexOfFirst, 16)
+col = 0
+row=0
+for filename in filenames:
+	if col==16:
+		col =0
+		row=row+1
+	increment = col*16
+	hexnum = hex(decnum+row+increment)[2:].upper();
+	if len(hexnum) == 2:
+		hexnum = '00' + hexnum;
+	if len(hexnum) == 3:
+		hexnum = '0' + hexnum;
+	#print '>'+hexnum;	
+	newname = hexnum + '.png';
+	print filename, ' becomes ', newname
+	os.rename( filename, newname );
+	#decnum = decnum+1;
+	col = col+1
+	print decnum;
+	
---
+++ slices/workarea/small/fname2hex58.py
@@ -0,0 +1,37 @@
+import os, string, glob, sys
+
+if len(sys.argv) > 1:
+    decnum = sys.argv[1];
+else:
+    print 'Hex number of first character:'
+    decnum = sys.stdin.readline()[:-1];
+
+filenames = glob.glob('*.png')
+for filename in filenames:
+	if len(filename) == 41: 
+		correctedname = filename[:35]+'0'+filename[35:]
+		os.rename( filename, correctedname );
+
+
+filenames = glob.glob('*.png')
+decnum = int(decnum, 16)
+col = 0
+row=0
+for filename in filenames:
+	if col==29:
+		col =0
+		row=row+1
+	increment = col*16
+	hexnum = hex(decnum+row+increment)[2:].upper();
+	if len(hexnum) == 2:
+		hexnum = '00' + hexnum;
+	if len(hexnum) == 3:
+		hexnum = '0' + hexnum;
+	#print '>'+hexnum;	
+	newname = hexnum + '.png';
+	print filename, ' becomes ', newname
+	os.rename( filename, newname );
+	#decnum = decnum+1;
+	col = col+1
+	print decnum;
+	
